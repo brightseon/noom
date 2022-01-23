b26e4b470dbd28d86cf7df3d7d7eaab4ff0984fb
brightseon <public.seon@gmail.com>
2022-01-23 14:34:28 +0900

    2-8 Room Count part One

--- src/server.js
+++ src/server.js
@@ -17,6 +17,18 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
+function publicRooms() {
+    const { sockets : { adapter : { sids, rooms } } } = wsServer;
+    const publicRooms = [];
+    rooms.forEach((_, key) => {
+        if(sids.get(key) === undefined) {
+            publicRooms.push(key);
+        }
+    });
+
+    return publicRooms;
+};
+
 wsServer.on('connection', socket => {
     socket['nickname'] = 'Anon';
     socket.onAny(event => console.log(`Socket Event: ${ event }`));
