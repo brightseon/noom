f5551c49559e68e6aa2282cc696e32a51e3a7c05
brightseon <public.seon@gmail.com>
2022-01-23 14:01:43 +0900

    2-6 Room Notifications

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -15,11 +15,23 @@ function addMessage(message) {
     ul.appendChild(li);
 };
 
+function handleMessageSubmit(event) {
+    event.preventDefault();
+    const input = room.querySelector('input');
+    const value = input.value;
+    socket.emit('new_message', input.value, roomName, () => {
+        addMessage(`You: ${ value }`);
+    });
+    input.value = '';
+};
+
 function showRoom() {
     welcome.hidden = true;
     room.hidden = false;
     const h3 = room.querySelector('h3');
     h3.innerText = `Room ${ roomName }`;
+    const form = room.querySelector('form');
+    form.addEventListener('submit', handleMessageSubmit);
 };
 
 function handleRoomSubmit(event) {
@@ -35,3 +47,9 @@ form.addEventListener('submit', handleRoomSubmit);
 socket.on('welcome', () => {
     addMessage('Someone joined!');
 });
+
+socket.on('bye', () => {
+    addMessage('Someone left ㅠㅠ');
+});
+
+socket.on('new_message', addMessage);
