ed61df2a432e1d4a6c20ea7290321e149d72918d
brightseon <public.seon@gmail.com>
2022-01-24 20:04:40 +0900

    2-10 User Count

--- src/server.js
+++ src/server.js
@@ -29,17 +29,23 @@ function publicRooms() {
     return publicRooms;
 };
 
+function countRoom(roomName) {
+    return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+};
+
 wsServer.on('connection', socket => {
     socket['nickname'] = 'Anon';
     socket.onAny(event => console.log(`Socket Event: ${ event }`));
     socket.on('enter_room', (roomName, done) => {
         socket.join(roomName);
         done();
-        socket.to(roomName).emit('welcome', socket.nickname);
+        socket.to(roomName).emit('welcome', socket.nickname, countRoom(roomName));
         wsServer.sockets.emit('room_change', publicRooms());
     });
     socket.on('disconnecting',  () => {
-        socket.rooms.forEach(room => socket.to(room).emit('bye', socket.nickname));
+        socket.rooms.forEach(room => 
+            socket.to(room).emit('bye', socket.nickname, countRoom(room) - 1)
+        );
     });
     socket.on('disconnect', () => {
         wsServer.sockets.emit('room_change', publicRooms());
