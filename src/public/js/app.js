a1350508b7bd22d67d4cfcb06c7b490ae2e945f7
brightseon <public.seon@gmail.com>
2022-01-27 22:32:05 +0900

    3-9 STUN

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -143,7 +143,17 @@ socket.on('ice', ice => {
 
 // RTC Code
 function makeConnection() {
-    myPeerConnection = new RTCPeerConnection();
+    myPeerConnection = new RTCPeerConnection({
+        iceServers : [{
+            urls : [
+                'stun:stun.l.google.com:19302',
+                'stun:stun1.l.google.com:19302',
+                'stun:stun2.l.google.com:19302',
+                'stun:stun3.l.google.com:19302',
+                'stun:stun4.l.google.com:19302'
+            ]
+        }]
+    });
     myPeerConnection.addEventListener('icecandidate', handleIce);
     myPeerConnection.addEventListener('addstream', handleAddStream);
     myStream.getTracks().forEach(track => myPeerConnection.addTrack(track, myStream));
